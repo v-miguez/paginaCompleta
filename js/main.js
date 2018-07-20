6a2cfa41dd4c6bdc6cea0df842b82ca6809a27c2
Victor Miguez Vega <v_miguez@outlook.es>
2018-07-20 14:07:08 +0200

    añadido filtrado de imagenes a traves del data escrito en ellas

--- js/main.js
+++ js/main.js
@@ -7,9 +7,10 @@
 		json.addEventListener('readystatechange', cargarLista);
 		var numeroMenus =new Array();
 		var listaImagenes= new Array();
+		var listaAlbum=new Array();
+		var listaIds=new Array();
+
 		$('html, body').ready(cargarLista);
-
-
 
 
 
@@ -21,17 +22,19 @@
 			if (this.readyState==4 && this.status == 200){
 
 				var lista = JSON.parse(this.responseText);
-				for(var i = 1; i<200; i=i+50){
+				for(var i = 1; i<100; i=i+50){
 
 					numeroMenus.push(lista[i]['albumId']);
 
 				}
-				for (var j= 0; j<200; j++){
-					listaImagenes.push(lista[i]['url']);
+				for (var j= 0; j<100; j++){
+					listaImagenes.push(lista[j]['thumbnailUrl']);
+					listaAlbum.push(lista[j]['albumId']);
+					listaIds.push(lista[j]['id']);
 
 				}
 				pintarMenu(numeroMenus);
-				pintarFoto(listaImagenes);
+				pintarFoto(listaImagenes, listaAlbum, listaIds);
 			}
 		}
 
@@ -40,18 +43,21 @@
 			var menu="";
 			for ( var j=1; j<=pNumeroMenus.length; j++ ){
 
-				menu +='<li>menu'+j+'</li>';
+				menu +='<li class="enlace" id="'+j+'">menu'+j+'</li>';
 			}
 			$('#listado').html(menu);
+
+			//listeners del listado
+			$('#listado li').on("click", filtrarFoto);
+
 		}
 
 
 
-		function pintarFoto(pListaImagenes){
+		function pintarFoto(pListaImagenes, pListaTipo, pListaId){
 			var imagenes= "";
 			for(var k=0; k<pListaImagenes.length; k++){
-
-				imagenes +='<img src="'+pListaImagenes[k]+'" class="imagen"></img>'
+				imagenes +='<img src="'+pListaImagenes[k]+'" class="imagen" data-album="'+pListaTipo[k]+'" id="'+pListaId[k]+'"></img>'
 			}
 				$('#zonaImagenes').html(imagenes);
 
@@ -60,5 +66,9 @@
 
 
 
+		function filtrarFoto(event){
+			$('.imagen').css('display', 'none');
+			$('*[data-album="'+this.id+'"]').css('display', 'flex');
 
+		}
 
